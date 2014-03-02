8cc4d1698986cb9758c79ab26813ef09679c588d
olivier5741 <olivier5741@gmail.com>
2014-03-03 00:37:03 +0100

    RavenDB - part 1

--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Raven.Client.Document;
+using Raven.Client.Indexes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -10,12 +13,20 @@ namespace WebApplication
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static DocumentStore Store;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //RavenDB
+            Store = new DocumentStore { ConnectionStringName = "RAVENHQ_CONNECTION_STRING" };
+            Store.Initialize();
+
+            IndexCreation.CreateIndexes(Assembly.GetCallingAssembly(), Store);
         }
     }
 }

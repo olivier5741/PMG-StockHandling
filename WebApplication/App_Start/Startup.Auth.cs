aea2d25fbfd1143757e4a0be7d16710ece7e82fb
olivier5741 <olivier5741@gmail.com>
2014-03-03 00:00:44 +0100

    Add Google authentification

--- WebApplication/App_Start/Startup.Auth.cs
+++ WebApplication/App_Start/Startup.Auth.cs
@@ -32,7 +32,7 @@ namespace WebApplication
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication();
+            app.UseGoogleAuthentication();
         }
     }
 }
